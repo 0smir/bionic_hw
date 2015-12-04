3629ea0ff05280f1f3eb491f9b775cd31543e3c4
Olga Smirnova <koshechka_ma@mail.ru>
2015-12-04 13:54:18 +0200

    add script for selectors

---
+++ jquary/jq/exercise/selector/style.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+
+	$('#one').click(function(){$('#one>p').hide();});
+
+	$('#two').click(function(){$('#test').hide();});
+
+	$('#even').click(function(){$('#three>h2:even').hide();});
+
+	$('#four>p').click(function(){$(this).hide();});
+
+	$('#five>p').dblclick(function(){$(this).hide();});
+
+	$('table').mouseenter(function(){$('tr:odd').css('background-color','#f0f0f0');
+	});
+
+});
