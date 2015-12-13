b37f0c65ddef1adcf4eb702cf227ae658ef5d71c
Olga Smirnova <koshechka_ma@mail.ru>
2015-12-14 00:17:47 +0200

    add styles for indicator

--- graduate/carousel.js
+++ graduate/carousel.js
@@ -1,36 +1,12 @@
 
 $(document).ready(function(){
-    $(function() {
 
-        // Cycles to the previous item
-        $(".prev-slide").click(function() {
-            $("#myCarousel").carousel('prev');
-        });
+    //for images
 
-        // Cycles to the next item
-        $(".next-slide").click(function() {
-            $("#myCarousel").carousel('next');
-        });
-
-        // Cycles the carousel to a particular frame
-        $(".slide-one").click(function() {
-            $("#myCarousel").carousel(0);
-        });
-
-        $(".slide-two").click(function() {
-            $("#myCarousel").carousel(1);
-        });
-
-        $(".slide-three").click(function() {
-            $("#myCarousel").carousel(2);
-        });
-
-        $(".slide-three").click(function() {
-            $("#myCarousel").carousel(4);
-        });
+    $(function()
+        $('carousel').on('slide.bs.carousel', function () {
+            $('.active>img').animate({ opacity: '0.4' }, 2000);});
     });
-
-
 
 
 });
