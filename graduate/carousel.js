9dedca400e138dcc59c26fd6b01daf7fe6050cf0
Olga Smirnova <koshechka_ma@mail.ru>
2015-12-14 22:12:48 +0200

    add slider

--- graduate/carousel.js
+++ graduate/carousel.js
@@ -4,9 +4,27 @@ $(document).ready(function(){
     //for images
 
     $(function()
-        $('carousel').on('slide.bs.carousel', function () {
-            $('.active>img').animate({ opacity: '0.4' }, 2000);});
+        $('#main-proposition').on('slide.bs.carousel', function () {
+            $('#main-proposition.active>img').animate({ opacity: '0.4' }, "slow");});
     });
+
+
+});
+
+
+
+$(document).ready(function(){
+
+
+
+
+
+
+    $('#clients-slide').slick({
+        setting-name: setting-value
+});
+
+
 
 
 });
